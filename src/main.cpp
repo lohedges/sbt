779e14b55172167f5293f3877efab539172c3fa0
Lester Hedges <lester.hedges@gmail.com>
2022-03-01 12:27:26 +0000

    Allow user to specify number of IPUs on command-line.

--- src/main.cpp
+++ src/main.cpp
@@ -25,17 +25,58 @@
 #include "SearchByTripletIPU.h"
 
 /// Setup and connect to a Poplar IPU device.
-poplar::Device setIpuDeviceWithIpuModelFallBack(bool &is_model);
+poplar::Device setIpuDeviceWithIpuModelFallBack(bool &is_model, const int num_ipus);
 
-int main()
+int main(int argc, char *argv[])
 {
+    // Set the default number of IPUs to run on.
+    int num_ipus = 1;
+
+    // Get the number of IPUs from the command-line.
+    if (argc > 1)
+    {
+        std::string arg = argv[1];
+
+        try
+        {
+            std::size_t pos;
+            num_ipus = std::stoi(arg, &pos);
+            if (pos < arg.size())
+            {
+                std::cerr << "Trailing characters after number of IPUs: " << arg << '\n';
+                exit(-1);
+            }
+        }
+        catch (std::invalid_argument const &ex)
+        {
+            std::cerr << "Invalid number of IPUs: " << arg << '\n';
+            exit(-1);
+        }
+        catch (std::out_of_range const &ex)
+        {
+            std::cerr << "Number of IPUs out of range: " << arg << '\n';
+            exit(-1);
+        }
+    }
+
+    if (num_ipus > 4)
+    {
+        std::cerr << "Number of IPUs must be in range 1-4.\n";
+        exit(-1);
+    }
+
     // Read events from file.
     EventReader event_reader(std::filesystem::path("data"), true);
     event_reader.readEvents(true);
 
     // Setup the IPU device handle.
     bool is_model;
-    auto device = setIpuDeviceWithIpuModelFallBack(is_model);
+    auto device = setIpuDeviceWithIpuModelFallBack(is_model, num_ipus);
+
+    if (is_model and num_ipus > 1)
+    {
+        std::cout << "Running on an IPUModel. Ignoring 'num_ipus' option!\n";
+    }
 
     // Setup the graph program.
     std::cout << "Creating graph program...\n";
@@ -56,10 +97,10 @@ int main()
     return 0;
 }
 
-poplar::Device setIpuDeviceWithIpuModelFallBack(bool &is_model)
+poplar::Device setIpuDeviceWithIpuModelFallBack(bool &is_model, const int num_ipus)
 {
     auto dm = poplar::DeviceManager::createDeviceManager();
-    auto hwDevices = dm.getDevices(poplar::TargetType::IPU, 1);
+    auto hwDevices = dm.getDevices(poplar::TargetType::IPU, num_ipus);
     if (hwDevices.size() > 0)
     {
         for (auto &d : hwDevices)
